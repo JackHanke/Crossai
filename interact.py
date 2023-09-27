0a2540b000635f158bc0130eda3016697370f2d8
JackHanke <jack.hanke98@gmail.com>
2023-09-27 14:59:05 -0400

    Score from move history, cropped digits included

--- interact.py
+++ interact.py
@@ -3,8 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
-from PIL import Image
-from process import reward, feed_forward
+from PIL import Image, ImageOps, ImageChops
+from process import reward
+from random import randint
 
 driver = webdriver.Firefox()
 driver.get("https://poki.com/en/g/crossy-road")
@@ -26,28 +27,38 @@ game_window = driver.find_element(by=By.XPATH, value='/html/body')
 
 move_dict = {0:None, 1:Keys.UP, 2:Keys.RIGHT, 3:Keys.DOWN, 4:Keys.LEFT}
 
-# sleep(2)
-
 move = 0
 score = 0
-while score >= 0:
+defecit=0
+moves=[]
+ 
+game_window.send_keys(move_dict[2])
+sleep(1)
+
+while score >= -10:
     if move != 0:
         game_window.send_keys(move_dict[move])
 
     # screenshot game (needs optimizing)
     driver.switch_to.default_content()
     screenshot = driver.get_screenshot_as_file("image.png")
+    #screenshot = ImageOps.grayscale(color_screenshot)
     screenshot_window = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[2]/div[1]/div[1]/div/div/div")
     screenshot_loc, screenshot_size = screenshot_window.location, screenshot_window.size
     x, y = screenshot_loc['x'], screenshot_loc['y']
-    w, h = x + screenshot_size['width'], y + screenshot_size['height']
-    screenshot = Image.open("./image.png")
+    w, h = x + screenshot_size['width'], y + screenshot_size['height']-64 # correction for white bar
+    screenshot = Image.open(".\\image.png") #os path this
     screenshot2 = screenshot.crop((x, y, w, h))
-    screenshot2.save('./image.png')
+    # screenshot2.save(f'.\\screenshots\\image{score}.png')
     driver.switch_to.frame(iframe)
     driver.switch_to.frame(inner_iframe)
 
-    score = reward(screenshot2)
-    move = feed_forward(screenshot2, score)
+    move = randint(0,4)
+    if move == 1 and defecit > 0: defecit -= 1
+    elif move == 3: defecit += 1
+    elif move == 1 and defecit == 0: score += 1
+    print(f"Move was: {move} Defecit is {defecit} Score is: {score}")
+    sleep(0.25)
+    
 
 driver.quit()
--- process.py
+++ process.py
@@ -1,11 +1,58 @@
 
+from PIL import Image, ImageChops
 from random import randint
 
-# function determines score from image, returns -999 if game over
-def reward(image):
-    return 0
+# reward records the score from the history of moves
+def reward(moves):
+    n = len(moves)
+    i = 0
+    while moves[n-1-i] != 1:
+        print(i,n)
+        i += 1
+        if i>= n-1: return sum(moves)
+    else: return sum(moves) + i
+    
 
-# function determines the next move from screenshot
-def feed_forward(image, score):
-    return randint(0,4)
+if __name__ == '__main__':
+    # deriving score from screen
+    example = Image.open(".\\screenshots\\image25.png") # 836 x 470
+
+    # os path this
+    zero = Image.open(".\\digits\\0-crop.jpg") 
+    one = Image.open(".\\digits\\1-crop.jpg") 
+    two = Image.open(".\\digits\\2-crop.jpg") 
+    three = Image.open(".\\digits\\3-crop.jpg") 
+    four = Image.open(".\\digits\\4-crop.jpg") 
+    five = Image.open(".\\digits\\5-crop.jpg") 
+    six = Image.open(".\\digits\\6-crop.jpg") 
+    seven = Image.open(".\\digits\\7-crop.jpg") 
+    eight = Image.open(".\\digits\\8-crop.jpg") 
+    nine = Image.open(".\\digits\\9-crop.jpg") 
+
+    # dif = ImageChops.difference(two, example)
+    # print(dif)
+
+    # all digits have 42 pixel width 42 except 1 which has width 25
+    print(example.width, example.height)
+    start_width=7
+    one_dig_loc = (7,start_width,7+25,start_width+42)
+    dig_loc = (7,start_width,7+42,start_width+42)
+    cropped = example.copy().crop(one_dig_loc)
+    cropped.save(".\\screenshots\\croppped.png")
+
+
+    # close opened images
+    zero.close()
+    one.close()
+    two.close()
+    three.close()
+    four.close()
+    five.close()
+    six.close()
+    seven.close()
+    eight.close()
+    nine.close()
+
+    example.close()
+
 
